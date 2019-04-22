6c7cf1a23b19fc1b349df32350f7b980a75d5e5a
brad <bjkapteyn@gmail.com>
2019-04-22 15:06:09 -0400

    Final Commit with working lab

--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -10,6 +10,100 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
+            bool isDouble(string dub)
+            {
+                if (double.TryParse(dub, out double x))
+                {
+                    return true;
+                }
+                else return false;
+            }
+            void Area()
+            {
+                Console.Clear();
+                string uLength;
+                string uWidth;
+                string uHeight;
+                string uQuit;
+                string quit;
+                double length;
+                double width;
+                double height;
+                double area;
+                double perimeter;
+                double volume;
+                while (true)
+                {
+                    Console.WriteLine("Enter Length: ");
+                    uLength = Console.ReadLine();
+                    if(isDouble(uLength))
+                    {
+                        length = Convert.ToDouble(uLength);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a number: ");
+                        continue;
+                    }
+                }
+                while (true)
+                {
+                    Console.WriteLine("Enter width: ");
+                    uWidth = Console.ReadLine();
+                    if (isDouble(uWidth))
+                    {
+                        width = Convert.ToDouble(uWidth);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a number: ");
+                        continue;
+                    }
+                }
+                area = length * width;
+                perimeter = (2 * length) + (2 * width);
+                Console.WriteLine($"Area:{area}" +
+                                  $"\nPerimeter: {perimeter}");
+                Console.WriteLine("Press any key to continue or q to quit");
+                uQuit = Console.ReadLine();
+                quit = uQuit.ToUpper();
+                if (quit == "Q")
+                {
+                    return;
+                }
+                while (true)
+                {
+                    Console.WriteLine("Enter height: ");
+                    uHeight = Console.ReadLine();
+                    if (isDouble(uHeight))
+                    {
+                        height = Convert.ToDouble(uHeight);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a number: ");
+                        continue;
+                    }
+                }
+                volume = length * width * height;
+                Console.WriteLine($"Volume: {volume}");
+                Console.WriteLine("Press R to restart or any key to quit");
+                uQuit = Console.ReadLine();
+                quit = uQuit.ToUpper();
+                if (quit == "R")
+                {
+                    Area();
+                }
+                else
+                {
+                    return;
+                }
+
+            }
+            Area();
         }
     }
 }
